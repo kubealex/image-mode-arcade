8c5292a0b151d7554c8468cb2db6e6de68df0b8e
kubealex <al.rossi87@gmail.com>
2026-06-26 18:19:13 +0200

    minor fixes

--- nesrom/retro_arcade.sh
+++ nesrom/retro_arcade.sh
@@ -1,4 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
 # Note: You need to download a NES ROM and place it in the same directory as the Containerfile
 # and replace "nesrom.nes" in this retro_arcade.sh and in the Containerfile with the filename of the nes ROM file.
+
+# Needed to prevent gnome from asking permissions - Uncomment if you are using this container image to deploy a VM using bootc-image-builder
+/usr/bin/flatpak permission-set gnome shortcuts-inhibitor org.mamedev.MAME.desktop GRANTED
+
+
 /usr/bin/flatpak run org.libretro.RetroArch -L /opt/retroarch/cores/nestopia_libretro.so /opt/retroarch/roms/nesrom.nes
